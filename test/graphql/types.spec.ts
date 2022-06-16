feb0f809de1b36167f6195181b01e41817102d3b
Caique Araujo <caique@studiopiggly.com.br>
2022-06-16 20:36:56 -0300

    :rotating_light: test(graphql): Types integrity

---
+++ test/graphql/types.spec.ts
@@ -0,0 +1,102 @@
+import request from 'supertest';
+import { ApiHelper } from '@test/utils';
+
+// @ts-ignore
+const server: ApiHelper = global.api;
+let requestMock: request.SuperTest<request.Test>;
+
+beforeAll(async () => {
+	await server.bootstrap();
+	requestMock = await server.open();
+});
+
+afterAll(async () => {
+	await server.close();
+});
+
+describe('GraphQL integrity types', () => {
+	it('Query types', async () => {
+		const { body } = await requestMock.post('/graphql').send({
+			query: `
+				{
+					__type(name: "Query") {
+						fields {
+							name
+						}
+					}
+				}`,
+		});
+
+		expect(body.data.__type.fields.length).toBe(2);
+		expect(body.data.__type.fields).toStrictEqual([
+			{
+				name: 'getAllMovies',
+			},
+			{
+				name: 'getMovie',
+			},
+		]);
+	});
+
+	it('Movie types', async () => {
+		const { body } = await requestMock.post('/graphql').send({
+			query: `
+				{
+					__type(name: "Movie") {
+						fields {
+							name
+						}
+					}
+				}`,
+		});
+
+		expect(body.data.__type.fields.length).toBe(15);
+		expect(body.data.__type.fields).toStrictEqual([
+			{
+				name: 'vote_count',
+			},
+			{
+				name: 'id',
+			},
+			{
+				name: 'video',
+			},
+			{
+				name: 'vote_average',
+			},
+			{
+				name: 'title',
+			},
+			{
+				name: 'popularity',
+			},
+			{
+				name: 'poster_path',
+			},
+			{
+				name: 'original_language',
+			},
+			{
+				name: 'original_title',
+			},
+			{
+				name: 'genre_ids',
+			},
+			{
+				name: 'backdrop_path',
+			},
+			{
+				name: 'adult',
+			},
+			{
+				name: 'overview',
+			},
+			{
+				name: 'release_date',
+			},
+			{
+				name: 'most_popular',
+			},
+		]);
+	});
+});
